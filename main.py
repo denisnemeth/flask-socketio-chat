ddbaaf8b6521873179d474e272d0974b12226ef9
denisnemeth <55528169+denisnemeth@users.noreply.github.com>
2021-06-12 13:36:56 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -3,12 +3,14 @@ from flask_socketio import SocketIO, send
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'mysecret'
-socketio = SocketIO(app, cors_allowed_origins='*')
+socketio = SocketIO(app, ping_timeout=3, ping_interval=1)
 
 @socketio.on('message')
 def handleMessage(msg):
 	print('Message: ' + msg)
 	send(msg, broadcast=True)
+
+#import logging
 
 if __name__ == '__main__':
 	socketio.run(app)
